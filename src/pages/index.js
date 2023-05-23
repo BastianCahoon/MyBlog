9ba1a92d5c9d642bc8ff8f0f7969c9cb12729401
BastianCahoon <bastian.cahoon@gmail.com>
2023-05-23 11:35:12 -0600

    Added image to the Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,17 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="A dog looking suspiciously at the camera"
+        src="https://as2.ftcdn.net/v2/jpg/02/71/09/23/1000_F_271092396_XcvyoBNcm8ttWRa0mM6j3ZISBMmy4X4S.jpg"
+      />
     </Layout>
   )
 }
